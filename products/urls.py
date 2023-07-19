3807d31e5625174861636d956681e47399495e4c
assofiejakobsson <s89j@live.se>
2023-07-19 01:58:24 +0000

    Added content to main-nav.html and mobile-top-header.template. Started the products template. An made som changes smale changes in the templates, settings and the urls.

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'products'
+
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
 ]
