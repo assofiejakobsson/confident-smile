0122158d8415e18b2a871588a7a4377fa6aa88b7
assofiejakobsson <s89j@live.se>
2023-07-20 02:22:28 +0000

    Added and changed some links. And Added seartch functionality.

--- confident_smile/urls.py
+++ confident_smile/urls.py
@@ -18,10 +18,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+""" urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
+    path('products/', include('products.urls', namespace='products')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) """
