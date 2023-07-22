cc8aa356eeb84b34eaefe303fcb0a190098f63b0
assofiejakobsson <s89j@live.se>
2023-07-22 21:29:46 +0000

    updated context procesor and added it to the bag template.

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,27 +1,46 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
 
-    """ 
-    Gratis leverans eller inte beroende på beställnings summan
-    if total < settings.FREE_DELIVERY_THRESHOLD:
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+
+    
+    # Gratis leverans eller inte beroende på beställnings summan
+    """ if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
-        free_delivery_delta = 0 
+        free_delivery_delta = 0  """
+
+    delivery = Decimal('10.00')
     
-    grand_total = delivery + total """
+    grand_total = delivery + total 
     
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
+        'delivery': delivery,
+        'grand_total': grand_total,
     }
 
     return context
