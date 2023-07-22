9ce4cae8789ef0cd14f9f94279d626955b6122ab
assofiejakobsson <s89j@live.se>
2023-07-22 02:18:13 +0000

    Added bag context.

--- confident_smile/settings.py
+++ confident_smile/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -168,3 +169,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+""" 
+jag har ingen banner till detta men har det i contexts.py
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10 """
