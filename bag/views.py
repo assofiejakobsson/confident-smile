a4841ed93f5a1ebf20466b890e0565a9ea4c3dfa
assofiejakobsson <s89j@live.se>
2023-07-24 14:08:46 +0000

    Added confirm message in the shopping bag.

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.urls import reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -11,6 +11,7 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -19,6 +20,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
