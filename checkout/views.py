16a45011715928fe1eb3203279c1516a8b67b169
assofiejakobsson <s89j@live.se>
2023-07-28 16:08:44 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NYcs2IgkEUJ9Ck873dAwr1VYG2Rn6MwgNijLWrlPRzFgq45Zh1FGpzY99Q6muzlkzH9lvokK9CtXL5758xHQFnc00gQXV9XC7',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
