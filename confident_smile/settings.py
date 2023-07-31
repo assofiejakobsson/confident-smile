63c678535d19e8fac2e2cfd5b6f189a9a2dafa4b
assofiejakobsson <s89j@live.se>
2023-07-31 17:16:05 +0000

    Updated webhook_handeler whit profiles. And added conformation emails.

--- confident_smile/settings.py
+++ confident_smile/settings.py
@@ -192,11 +192,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Stripe
 # FREE_DELIVERY_THRESHOLD = 50
-STANDARD_DELIVERY_PERCENTAGE = 10
+STANDARD_DELIVERY_PERCENTAGE = 0
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'confidentsmile@example.com'
 
 """ 
 jag har ingen banner till detta men har det i contexts.py
