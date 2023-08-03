d2b1666003d6454fdc031b0ca41a34a63feeffe0
assofiejakobsson <s89j@live.se>
2023-08-03 11:34:53 +0000

    Added a error template 404.

--- confident_smile/urls.py
+++ confident_smile/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from .views import handler404
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -29,6 +30,8 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
+handler404 = 'confident_smile.views.handler404'
 
 """ urlpatterns = [
     path('admin/', admin.site.urls),
