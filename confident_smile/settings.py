ce8f43bdc99191035fa82ba547ec8a4acc2b2d33
assofiejakobsson <s89j@live.se>
2023-09-17 15:41:11 +0000

    Updated all buttons.

--- confident_smile/settings.py
+++ confident_smile/settings.py
@@ -73,6 +73,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
